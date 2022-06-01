f35a1b22b7b70c9227993f41b26ce32d321e1f73
youus <alecshen0926@foxmail.com>
2022-06-01 23:54:54 +0800

    🎉 init fileds web

---
+++ src/pages/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  1213
+</template>
+
+<script lang="ts">
+/**
+ * @Description
+ * @Author youus
+ * @Date 21:46
+ * @Version v1.0.0
+ *
+ * Hello, humor
+ */
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Main',
+  setup() {}
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+/**
+ * @Description
+ * @Author youus
+ * @Date 2022/6/1 21:42
+ * @Version v1.0.0
+ *
+ * Hello, humor
+ */
+
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'Main',
+      component: import(/* webpackChunkName: "main" */ '../pages/Main.vue'),
+    },
+  ],
+});
+
+export default router;
